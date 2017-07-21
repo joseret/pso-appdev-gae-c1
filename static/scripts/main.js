6fc70a066cedaefba7f7f7ed73206bb7a0daf254
Jose <joseret@google.com>
2017-07-20 20:45:11 -0700

    Now we can log in with firebase, onto the server

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -21,15 +21,65 @@ function FriendlyChat() {
   this.signInButton = document.getElementById('sign-in');
   this.signInButton.addEventListener('click', this.signInWithPopup.bind(this));
 
+  this.submitButton = document.getElementById('submit');
+
+  this.userPic = document.getElementById('user-pic');
+  this.userName = document.getElementById('user-name');
+  this.signOutButton = document.getElementById('sign-out');
+  this.signOutButton.addEventListener('click', this.signOut.bind(this));
+  this.signInSnackbar = document.getElementById('must-signin-snackbar');
+
+
   this.initFirebase();
 }
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
 FriendlyChat.prototype.initFirebase = function() {
   // TODO(DEVELOPER): Initialize Firebase.
-  this.signInButton.removeAttribute('hidden');
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  // this.database = firebase.database();
+  // this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
+FriendlyChat.prototype.onAuthStateChanged = function(user) {
+
+  if (user) { // User is signed in!
+    console.log('user', user);
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = user.photoURL; // Only change these two lines!
+    var userName = user.displayName;
+    var self = this;
+    user.getToken().then(function(idToken) {
+      console.log('getToken', idToken, user);
+      self.userIdToken = idToken;
+    });
+    // Set the user's profile pic and name.
+    this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+    this.userName.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    this.userName.removeAttribute('hidden');
+    this.userPic.removeAttribute('hidden');
+    this.signOutButton.removeAttribute('hidden');
+     // Hide sign-in button.
+    this.signInButton.setAttribute('hidden', 'true');
+
+
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    this.idUserToken = null;
+    this.userName.setAttribute('hidden', 'true');
+    this.userPic.setAttribute('hidden', 'true');
+    this.signOutButton.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    this.signInButton.removeAttribute('hidden');
+
+  }
+};
 
 FriendlyChat.prototype.signInWithPopup = function() {
   window.open(this.getWidgetUrl(), 'Autenticar', 'width=1100,height=735');
@@ -43,6 +93,11 @@ FriendlyChat.prototype.getWidgetUrl = function()  {
 };
 
 
+// Signs-out of Friendly Chat.
+FriendlyChat.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
 
 // A loading image URL.
 FriendlyChat.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
